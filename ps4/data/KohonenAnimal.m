15448e360eabcedde1a403d083d27b1bd9a4e144
zyu47 <yuzhixian1990@gmail.com>
2018-04-16 20:07:31 -0600

    initial commit

---
+++ ps4/data/KohonenAnimal.m
@@ -0,0 +1,51 @@
+%description of the data: M(i,j) = attribute i, animal j   
+%i=1:13
+
+%j=1:16
+%Dove 	 
+%Hen 	 
+%Duck 	 
+%Goose 	 
+%Owl 	 
+%Hawk 	 
+%Eagle 	 
+%Fox 	 
+%Dog 	 
+%Wolf 	 
+%Cat 	 
+%Tiger 	 
+%Lion 	 
+%Horse 	 
+%Zebra 	 
+%Cow
+
+%rows: 
+%small size
+%medium size
+%large size
+%2 legs
+%4 legs
+%hair
+%hooves
+%mane
+%feathers
+%hunts
+%runs
+%flys
+%swims
+
+%the number one indicates that animal j possesses attribute i and zero indicates it does not.
+
+M = [1 	1 	1 	1 	1 	1 	0 	0 	0 	0 	1 	0 	0 	0 	0 	0;
+0 	0 	0 	0 	0 	0 	1 	1 	1 	1 	0 	0 	0 	0 	0 	0;
+0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	1 	1 	1 	1 	1;
+1 	1 	1 	1 	1 	1 	1 	0 	0 	0 	0 	0 	0 	0 	0 	0;
+0 	0 	0 	0 	0 	0 	0 	1 	1 	1 	1 	1 	1 	1 	1 	1;
+0 	0 	0 	0 	0 	0 	0 	1 	1 	1 	1 	1 	1 	1 	1 	1;
+0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	1 	1 	1;
+0 	0 	0 	0 	0 	0 	0 	0 	0 	1 	0 	0 	1 	1 	1 	0;
+1 	1 	1 	1 	1 	1 	1 	0 	0 	0 	0 	0 	0 	0 	0 	0;
+0 	0 	0 	0 	1 	1 	1 	1 	0 	1 	1 	1 	1 	0 	0 	0;
+0 	0 	0 	0 	0 	0 	0 	0 	1 	1 	0 	1 	1 	1 	1 	0;
+1 	0 	0 	1 	1 	1 	1 	0 	0 	0 	0 	0 	0 	0 	0 	0;
+0 	0 	1 	1 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0 	0]
